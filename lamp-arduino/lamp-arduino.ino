565829a269d6d92734a950d4c6960106395fc0dd
Johan Kladder <johankladder@gmail.com>
2020-05-17 14:54:28 +0200

    + Added FastLED and support for WS2812B leds

--- lamp-arduino/lamp-arduino.ino
+++ lamp-arduino/lamp-arduino.ino
@@ -1,35 +1,48 @@
 #include <SoftwareSerial.h>
+#include <FastLED.h>
 
+// Pin setup:
 SoftwareSerial HM10(0, 1); 
-String command = "";
-int ledPin = 3;
+#define NUM_LEDS 12
+#define DATA_PIN 3
 
-// Serial BT input data storage:
-const byte numChars = 32;
+// Storage for the LED information - like color
+CRGB leds[NUM_LEDS];
+
+// Maximum number of input chars. As otherwise
+// the Arduino reads max String length. This improves speed. 
+const byte numChars = 32; 
 char receivedChars[numChars];
 boolean newData = false;
 
+// The main color of the leds:
+CRGB color;
+
 void setup()
 {
+  // Add all the leds to the program. Please be aware of powerconsumption: 60ma per pixel!
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS); 
+
+  // Begin serial and BT communication:
   Serial.begin(9600);
+  HM10.begin(9600);
   Serial.println("HM10 serial started at 9600");
-  HM10.begin(9600); // set HM10 serial at 9600 baud rate
 
+  // Default color of the leds is GREEN:
+  color = CRGB::Green;
 
-  pinMode(ledPin, OUTPUT); // onboard LED
-  digitalWrite(ledPin, LOW); // switch OFF LED
-
+  // Turn all leds off to clear previous states:
+  FastLED.clear();
+  FastLED.show();
 }
 
 void loop()
 {
-    HM10.listen();  // listen the HM10 port
-    
+    HM10.listen();
     readData();
-    showData(); // Show data for debug purposes
+    showData();
     processData();
-
-    newData = false; // Reset flag 
+    newData = false;
 }
 
 void readData() {
@@ -65,13 +78,9 @@ void processData() {
     if (newData == true) {
       String command = receivedChars; 
       String preCommand = command.substring(0,3);
-  
+
       if (preCommand == "CML") { 
           lightsState(command.charAt(3));
-      }
-  
-      if (preCommand == "CMB") {
-           lightsBlink(command.charAt(3));
       }
 
       if (preCommand == "CMD") {
@@ -91,25 +100,26 @@ void lightsState(char value) {
 
 
 void lightsOn() {
-   digitalWrite(ledPin, HIGH); 
+    setAllLedsColor(color);
+    FastLED.show(); 
+}
+
+void setAllLedsColor(CRGB color) {
+    for (int led = 0; led < NUM_LEDS; led++) {
+        setLedColor(led, color);
+    }
+}
+
+void setLedColor(int led, CRGB color) {
+    leds[led] = color;
 }
 
 void lightsOff() {
-  digitalWrite(ledPin, LOW);
-}
-
-void lightsBlink(char speedChar) {
-  String s = "";
-  s += speedChar;
-  int intSpeed = s.toInt(); // int = 1 to 9
-  
-  digitalWrite(ledPin, LOW);
-  digitalWrite(ledPin, HIGH);
-  delay(1000 / intSpeed);
-  digitalWrite(ledPin, LOW);
-  delay(1000 / intSpeed);
+    FastLED.clear();
+    FastLED.show();
 }
 
 void setBrightness(int brightness) {
-    analogWrite(ledPin, brightness);
+    FastLED.setBrightness(brightness);
+    FastLED.show();
 }
